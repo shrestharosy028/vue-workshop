d8ccc030221ce07999a750912086a1b6197c3ae3
Rosy <rosy@fusemachines.com>
2019-09-26 14:53:59 +0545

    [EXAMPLE] : Basic hello world example

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    Hello world {{message}}
+    <p v-text="message"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "helloWorld",
+  data: function() {
+    return {
+      message: "Vue is awesome"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
+
